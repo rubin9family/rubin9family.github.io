d48b5469740af821eb3dd4ec7a8357baaacb637e
rubin9family <102113251+rubin9family@users.noreply.github.com>
2026-02-08 19:05:11 -0500

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,8 +1,8 @@
 function copyadd() {
  navigator.clipboard.writeText("55 Wormwood Hill Road, Wormwood Hills NJ, 00411");
- alert(“Copied Address to clipboard");
+ alert("Copied Address to clipboard");
 }
 function copytel() {
  navigator.clipboard.writeText("2012012012");
- alert(“Copied Telephone Number to clipboard");
+ alert("Copied Telephone Number to clipboard");
 }
